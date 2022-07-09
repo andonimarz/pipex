f5e30b7042da6fd0e7503b3813aa2af843c35ba5
Roscochof <andonimc92@hotmail.com>
2022-07-09 04:09:06 +0200

    split modified. now pipex handles single quotes

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -5,14 +5,14 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: amarzana <amarzana@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/19 11:12:11 by amarzana          #+#    #+#             */
-/*   Updated: 2022/06/30 17:51:02 by amarzana         ###   ########.fr       */
+/*   Created: 2022/07/09 03:34:16 by amarzana          #+#    #+#             */
+/*   Updated: 2022/07/09 03:48:05 by amarzana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../pipex.h"
 
-static int	ft_coincidences(char const *s, char c)
+int	ft_coincidences(char const *s, char c)
 {
 	int	coin;
 	int	i;
@@ -21,48 +21,95 @@ static int	ft_coincidences(char const *s, char c)
 	coin = 0;
 	while (s[i])
 	{
-		while (s[i] == c)
-			i++;
-		if (s[i] != '\0')
+		if (s[i] != c && s[i] != 39)
 			coin++;
-		while (s[i] != c && s[i])
-		i++;
+		while (s[i] && (s[i] != c || s[i] == 39))
+		{
+			if (s[i] == 39)
+			{
+				coin++;
+				i++;
+				while (s[i] && s[i] != 39)
+					i++;
+			}
+			i++;
+		}
+		if (s[i])
+			i++;
 	}
 	return (coin);
 }
 
-char	**ft_split_job(char const *s, char c)
+char	*ft_make_str(char const *s, char c, int i)
 {
-	int		len;
-	int		i;
-	int		start;
-	int		coin;
-	char	**str;
+	char	*str_row;
+	int		counter;
 
-	coin = 0;
-	i = -1;
-	start = -1;
-	len = ft_strlen(s);
-	str = (char **) malloc(sizeof(char *) * (ft_coincidences(s, c) + 1));
-	if (str == NULL || sizeof(str) == 0)
-		return (NULL);
-	while (++i <= len)
+	counter = i;
+	if (s[counter++] == 39)
 	{
-		if (s[i] != c && start < 0)
-			start = i;
-		else if (start >= 0 && (s[i] == c || i == len))
-		{
-			str[coin++] = ft_substr(s, start, i - start);
-			start = -1;
-		}
+		while (s[counter] && s[counter] != 39)
+			counter++;
+		str_row = ft_substr(s, i + 1, counter - i - 1);
 	}
-	str[coin] = NULL;
-	return (str);
+	else
+	{
+		while (s[counter] && s[counter] != c)
+			counter++;
+		str_row = ft_substr(s, i, counter - i);
+	}
+	return (str_row);
+}
+
+char	*ft_get_row(char const *s, char c, int row)
+{
+	int		i;
+	int		counter;
+	char	*str_row;
+
+	str_row = NULL;
+	i = -1;
+	counter = -1;
+	while (++i < (int)ft_strlen(s) && s[i])
+	{
+		if (s[i] != c)
+			counter++;
+		if (counter == row && s[i] != c)
+		{
+			str_row = ft_make_str(s, c, i);
+			if (!str_row)
+				return (NULL);
+		}
+		while (s[i] && s[i] != c)
+			if (s[i++] == 39)
+				while (s[i] && s[i] != 39)
+					i++;
+	}
+	return (str_row);
 }
 
 char	**ft_split(char const *s, char c)
 {
+	char	**split;
+	int		coin;
+	int		i;
+
 	if (!s)
 		return (NULL);
-	return (ft_split_job(s, c));
+	coin = ft_coincidences(s, c);
+	if (c == '\0' && ft_strlen(s) > 0)
+		coin = 1;
+	split = (char **)malloc((coin + 1) * sizeof(char *));
+	if (split == NULL)
+		return (NULL);
+	i = 0;
+	while (i < coin)
+	{
+		split[i] = ft_get_row(s, c, i);
+		if (split[i] == NULL)
+			return (NULL);
+		i++;
+	}
+	split[i] = NULL;
+	return (split);
 }
